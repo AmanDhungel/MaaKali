ab0f37d5bcd4dd0df1d6bc47a7fd86b74fd8e640
Aman Dhungel <aman.most100@gmail.com>
2025-05-16 09:52:17 +0545

    added shadcn and components

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -22,8 +22,8 @@ const BlogCard = ({ blogs }: { blogs: Blog[] }) => {
     <>
       {blogs.map((blog) => (
         <article
-          className="bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
-          key={blog.id}>
+          key={blog.id}
+          className="bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
           <div className="relative h-48 w-full overflow-hidden">
             <Image
               width={500}
