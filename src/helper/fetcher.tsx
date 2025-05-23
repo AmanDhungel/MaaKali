972da0b96ffbac4f34b5329228f0dfdb7bf94733
Aman Dhungel <aman.most100@gmail.com>
2025-05-23 13:06:38 +0545

    fixed url problem

--- src/helper/fetcher.tsx
+++ src/helper/fetcher.tsx
@@ -1,7 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const BASE_URL = `${process.env.NEXT_API_URL}/api/`;
-
 interface ApiProps {
   url: string;
   params?: string;
@@ -9,7 +7,7 @@ interface ApiProps {
 
 export const GET = async <T,>(url: string): Promise<T> => {
   try {
-    const response = await axios.get<T>(`${BASE_URL}${url}`);
+    const response = await axios.get<T>(`/api/${url}`);
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -26,7 +24,7 @@ export const POST = async <T, R>(
   data: R,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  const response = await axios.post<T>(`${BASE_URL}${url}`, data, config);
+  const response = await axios.post<T>(`/api/${url}`, data, config);
   return response.data;
 };
 
@@ -35,7 +33,7 @@ export const PATCH = async <T, R>(
   data: R,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  const response = await axios.patch<T>(`${BASE_URL}${url}`, data, config);
+  const response = await axios.patch<T>(`/api/${url}`, data, config);
   return response.data;
 };
 
@@ -44,11 +42,11 @@ export const PUT = async <T, R>(
   data: R,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  const response = await axios.put<T>(`${BASE_URL}${url}`, data, config);
+  const response = await axios.put<T>(`/api/${url}`, data, config);
   return response.data;
 };
 
 export const DELETE = async <T,>(url: string): Promise<T> => {
-  const response = await axios.delete<T>(`${BASE_URL}${url}`);
+  const response = await axios.delete<T>(`/api/${url}`);
   return response.data;
 };
